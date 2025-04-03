33eba3be9f481202a9da9272d51a3d58cbdfab7e
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-04-03 10:34:08 -0300

    feat(home): add logout icon to header component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,12 +1,22 @@
+import { Power } from 'phosphor-react-native'
+import { TouchableOpacity } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
 import { Greeting, HomeHeaderContainer, Message, Name } from './styles'
 
 export const HomeHeader = () => {
+  const { COLORS } = useTheme()
+
   return (
     <HomeHeaderContainer>
       <Greeting>
         <Message>Olá,</Message>
         <Name>Lucas</Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power color={COLORS.GRAY_400} size={32} />
+      </TouchableOpacity>
     </HomeHeaderContainer>
   )
 }
